4c27e3faf7e8cf88727b03318fe3718ebe9df42b
Yaroslav Palamarchuk <superhabber@gmail.com>
2021-02-13 10:58:58 +0200

    f3

--- speech-control.js
+++ speech-control.js
@@ -1,20 +1,26 @@
 (function () {
-    if (annyang) {
 
-        var commands = {
+    fetchInject([
+        'https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js'
+    ]).then(() => {
+        if (annyang) {
 
-            'show me *tag': showFlickr
-        };
+            var commands = {
 
-        var showFlickr = function (tag) {
-            var url = 'http://api.flickr.com/services/rest/?tags=' + tag;
-            $.getJSON(url);
+                'show me *tag': showFlickr
+            };
+
+            var showFlickr = function (tag) {
+                var url = 'http://api.flickr.com/services/rest/?tags=' + tag;
+                $.getJSON(url);
+            }
+
+            // Add our commands to annyang
+            annyang.addCommands(commands);
+
+            // Start listening. You can call this here, or attach this call to an event, button, etc.
+            annyang.start();
         }
+    })
 
-        // Add our commands to annyang
-        annyang.addCommands(commands);
-
-        // Start listening. You can call this here, or attach this call to an event, button, etc.
-        annyang.start();
-    }
 })
