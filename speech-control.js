0b101ce659398c0b1e5408f9bcdf2136abd197f8
Yaroslav Palamarchuk <superhabber@gmail.com>
2021-02-13 10:31:42 +0200

    f

--- speech-control.js
+++ speech-control.js
@@ -1,12 +1,26 @@
 (function () {
 
-    fetchInject([
-        'https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js'
-    ]).then(() => {
+    function loadJs(url) {
+        return new Promise((resolve, reject) => {
+            if (document.querySelector(`head > script[ src = "${url}" ]`) !== null) {
+                console.warn(`script already loaded: ${url}`);
+                resolve();
+            }
+            const script = document.createElement("script");
+            script.src = url;
+            script.onload = resolve;
+            script.onerror = function (reason) {
+                // This can be useful for your error-handling code
+                reason.message = `error trying to load script ${url}`;
+                reject(reason);
+            };
+            document.head.appendChild(script);
+        });
+    }
 
-        console.log("YEAH")
-
+    loadJs("https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js").then(res => {
         if (annyang) {
+
             var commands = {
 
                 'show me *tag': showFlickr
@@ -23,7 +37,7 @@
             // Start listening. You can call this here, or attach this call to an event, button, etc.
             annyang.start();
         }
-    })
-
-
+    }).catch(err => {
+        console.log('An error occurred with annyang script loading. Error ' + err)
+    });
 })
