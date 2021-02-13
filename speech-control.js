263f9d4dac1cd4b9f5b623174473e43090202aa5
Yaroslav Palamarchuk <superhabber@gmail.com>
2021-02-13 10:35:42 +0200

    f1

--- speech-control.js
+++ speech-control.js
@@ -1,43 +1,27 @@
 (function () {
 
-    function loadJs(url) {
-        return new Promise((resolve, reject) => {
-            if (document.querySelector(`head > script[ src = "${url}" ]`) !== null) {
-                console.warn(`script already loaded: ${url}`);
-                resolve();
-            }
-            const script = document.createElement("script");
-            script.src = url;
-            script.onload = resolve;
-            script.onerror = function (reason) {
-                // This can be useful for your error-handling code
-                reason.message = `error trying to load script ${url}`;
-                reject(reason);
-            };
-            document.head.appendChild(script);
-        });
-    }
-
-    loadJs("https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js").then(res => {
-        if (annyang) {
-
-            var commands = {
-
-                'show me *tag': showFlickr
-            };
-
-            var showFlickr = function (tag) {
-                var url = 'http://api.flickr.com/services/rest/?tags=' + tag;
-                $.getJSON(url);
-            }
-
-            // Add our commands to annyang
-            annyang.addCommands(commands);
-
-            // Start listening. You can call this here, or attach this call to an event, button, etc.
-            annyang.start();
-        }
-    }).catch(err => {
-        console.log('An error occurred with annyang script loading. Error ' + err)
+    var x = document.createElement('script');
+    x.src = 'https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js';
+    document.getElementsByTagName("head")[0].appendChild(x).then(() => {
+        console.log("got it")
     });
+
+    if (annyang) {
+
+        var commands = {
+
+            'show me *tag': showFlickr
+        };
+
+        var showFlickr = function (tag) {
+            var url = 'http://api.flickr.com/services/rest/?tags=' + tag;
+            $.getJSON(url);
+        }
+
+        // Add our commands to annyang
+        annyang.addCommands(commands);
+
+        // Start listening. You can call this here, or attach this call to an event, button, etc.
+        annyang.start();
+    }
 })
